98dfbb8a4a54ed62f35f3e9b1899c64199207cb1
Kristóf <kristof.lacz@gmail.com>
2022-10-17 15:40:30 +0200

    merge

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,40 @@
+// let primitive1 = "alma";
+// let primitive2 = 145;
+// let primitive3 = false;
+
+// let array1 = [
+//     "alma", 
+//     "körte", 
+//     "olajbogyó"
+// ];
+
+// let array2 = [1, 5, 8];
+// let array3 = [false, true, false];
+
+// let newArray = [primitive1, primitive2, primitive3];
+// // console.log(newArray);
+
+// let object1 = {
+//     "key1": "value",
+//     "key2": 345,
+//     "key3": true,
+//     "key4": {
+//         nestedKey: "nestedValue", 
+//         nestedKey2: false
+//     },
+//     "key5": [
+//         "cica", "kutya", "mérési hiba", "jetilábnyom", "pizza"
+//     ]
+// }
+// // console.log(array1[2]);
+
+// console.log(object1.key5[4])
+
+/*ÓRAI FELADAT VÉGE*/
+
+
+
+
 // function loadEvent () { //
     const newPictureButton = document.querySelector(".add-pictures")
     newPictureButton.addEventListener("click", function () {
